2cb7f5f13fb2fc726eed73edeaa2e024ed9f90e5
JaeSunEo <wotjs020708@gmail.com>
2024-05-09 14:42:50 +0900

    Project JRNL

---
+++ JRNL/JournalListViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  JRNL
+//
+//  Created by 어재선 on 5/7/24.
+//
+
+import UIKit
+
+class JournalListViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
