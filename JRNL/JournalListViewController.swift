f5bd099dab9e15ca06cd7f9c0f2c730168d0f3f4
JaeSunEo <wotjs020708@gmail.com>
2024-05-09 16:08:33 +0900

    Exit segue

--- JRNL/JournalListViewController.swift
+++ JRNL/JournalListViewController.swift
@@ -7,15 +7,28 @@
 
 import UIKit
 
-class JournalListViewController: UIViewController {
-
-    @IBOutlet weak var tableView: UITableView!
-     
+class JournalListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       
+            tableView.dequeueReusableCell(withIdentifier: "journalCell", for: indexPath)
+
+    }
+    
+    @IBAction func unwindNewEntryCencel(segue: UIStoryboardSegue){
+        
+    }
 
 }
 
