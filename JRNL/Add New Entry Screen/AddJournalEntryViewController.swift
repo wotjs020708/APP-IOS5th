e7c91a169375db508b88a5a03d75e0e6506621c2
JaeSunEo <wotjs020708@gmail.com>
2024-05-10 14:03:10 +0900

    Interface Builder

--- JRNL/Add New Entry Screen/AddJournalEntryViewController.swift
+++ JRNL/Add New Entry Screen/AddJournalEntryViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class AddJournalEntryViewController: UIViewController {
 
+    @IBOutlet var titleTextField: UITextField!
+    @IBOutlet var bodyTextView: UITextView!
+    @IBOutlet var photoImageView: UIImageView!
+    
+    var newJournalEntry: JournalEntry?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,14 +22,15 @@ class AddJournalEntryViewController: UIViewController {
     }
     
 
-    /*
+    
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        
     }
-    */
+    
 
 }
