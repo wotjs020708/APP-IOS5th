2404c20f25b15b68ea805f0d43349448655e7e2e
JaeSunEo <wotjs020708@gmail.com>
2024-05-10 13:03:00 +0900

    Add TableViewCell

---
+++ JRNL/Journal List Screan/View/JournalListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  JournalListTableViewCell.swift
+//  JRNL
+//
+//  Created by 어재선 on 5/10/24.
+//
+
+import UIKit
+
+class JournalListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
