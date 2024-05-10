992a907feb31588f5ced32cb4b5c49fb2570bc0c
JaeSunEo <wotjs020708@gmail.com>
2024-05-10 13:45:58 +0900

    AddNewEntryController

---
+++ JRNL/Add New Entry Screen/AddJournalEntryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddJournalEntryViewController.swift
+//  JRNL
+//
+//  Created by 어재선 on 5/10/24.
+//
+
+import UIKit
+
+class AddJournalEntryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
