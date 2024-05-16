70bedfe5837b06a0eaa7ede476818b72e30d1532
JaeSunEo <wotjs020708@gmail.com>
2024-05-16 13:23:59 +0900

    RatingView 작성

--- JRNL/View/RatingView.swift
+++ JRNL/View/RatingView.swift
@@ -8,13 +8,40 @@
 import UIKit
 
 class RatingView: UIStackView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    private var ratingButtons = [UIButton()]
+    var rating = 0
+    private let buttonSize = CGSize(width: 44.0, height: 44.0)
+    private let buttonCount = 5
+    
+    // MARK: - Initialization
+    required init(coder: NSCoder){
+        super.init(coder: coder)
     }
-    */
+    
+    // MARK: - Private Methods
+    private func setupButtons() {
+        for button in ratingButtons {
+            removeArrangedSubview(button)
+            button.removeFromSuperview()
+        }
+        ratingButtons.removeAll()
+        let filledStar = UIImage(systemName: "star.fill")
+        let emptyStar = UIImage(systemName:  "star")
+        let highlightedStar = UIImage(systemName: "star.fill")?
+            .withTintColor(.red, renderingMode: .alwaysOriginal)
+        
+        for _ in 0..<buttonCount {
+            let button = UIButton()
+            button.setImage(emptyStar, for: .normal)
+            button.setImage(filledStar, for: .selected)
+            button.setImage(highlightedStar, for: .highlighted)
+            button.setImage(highlightedStar, for: [.highlighted, .selected])
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.widthAnchor.constraint(equalToConstant: buttonSize.width).isActive = true
+            button.heightAnchor.constraint(equalToConstant: buttonSize.height).isActive = true
+            addArrangedSubview(button)
+            ratingButtons.append(button)
+        }
+    }
 
 }
